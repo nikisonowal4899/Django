6dce048f469f8bda0557a41244311e3927153ff3
nikisonowal4899 <nikisonowal4899@gmail.com>
2019-01-12 22:24:54 +0530

    Django project1

--- mysite/music/urls.py
+++ mysite/music/urls.py
@@ -11,7 +11,13 @@ urlpatterns = [
     # /music/457/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
-    # music/album/add
+    # music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # music/album/2/
+    url(r'^album/(?P<pk>[0-9]+)/$', views.UpdateView.as_view(), name='album-update'),
+
+# music/album/2/delete/
+    url(r'^album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 
 ]
