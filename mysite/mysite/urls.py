b5badee3c048ecbab5d25d9c4e62b48a2661b031
nikisonowal4899 <nikisonowal4899@gmail.com>
2019-01-16 19:00:57 +0530

    Django project1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from home import views
-from django.conf.urls import url, include
-#Include is a package that lets us include other files
+from django.conf.urls import url, include #Include is a package that lets us include other files
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     url(r'^admin/', admin.site.urls),
     url(r'^music/', include('music.urls'), name='music'),
-    url(r'', views.home_view, name='home_view'),
+    #url(r'', views.home_view, name='home_view'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
